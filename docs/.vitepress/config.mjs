583caf0f3faec2ea0b924d07f5495120b3526c6f
xiangchuan <582266659@qq.com>
2024-08-16 23:40:27 +0800

    first

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,91 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from "./utils/auto_sidebar.mjs";
+
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: "/xiang-blog",
+  title: "小向 代码空间",
+  description: "小向 代码空间",
+  themeConfig: {
+    // sidebar: false, // 关闭侧边栏
+    // aside: "left", // 设置右侧侧边栏在左侧显示
+    outlineTitle: "引导",
+    "outline": [2, 6],
+    // logo: 'logo.png'
+    // https://vitepress.dev/reference/default-theme-config
+    // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
+    nav: [
+      { text: '学习指引', link: '/markdown-examples' },
+      {
+        text: 'JAVA',
+        items: [
+          { text: 'JAVA基础', link: '/java/Java基础.md' }
+        ]
+      }, {
+        text: 'MYSQL',
+        items: [
+          { text: 'MYSQL45讲', link: '/markdown-examples' }
+        ]
+      }, {
+        text: '书籍',
+        items: [
+          { text: '趣谈网络协议', link: '/book/趣谈网络协议.md' }
+        ]
+      }
+    ],
+
+    // sidebar: { "/书籍": set_sidebar("book") },
+
+    sidebar: {
+      "/java": {
+        text: 'Java',
+        items: [
+          {
+            text: 'Java',
+            items: [
+              { text: 'Java基础', link: '/java/Java基础.md' }
+            ]
+          }
+        ]
+      },
+      "/book": {
+        text: '书籍',
+        items: [
+          {
+            text: '书籍',
+            items: [
+              { text: '趣谈网络协议', link: '/book/趣谈网络协议.md' }
+            ]
+          }
+        ]
+      }
+
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://gitee.com/XiangChuan' }
+    ], footer: {
+      copyright: "Copyright @ 2024"
+    }
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "小向 代码空间"
+  text: "构建自己的代码空间"
+  tagline: 持续学习 持续进步
+  # image:
+  #   src: /back.svg
+  #   alt: 背景图片
+  actions:
+    - theme: brand
+      text: 学习指引
+      link: /markdown-examples
+    # - theme: alt
+    #   text: 零碎的知识
+    #   link: /api-examples
+
+features:
+  - title: 💡小建议
+    details: 选择永远大于努力。
+  - title: 🧗勇气
+    details: 心向阳光，无谓悲伤，用微笑迎接生活的挑战，用勇气书写成长的篇章。
+  - title: 🤔成长
+    details: 成长是异常孤独的旅行，唯有坚守内心的信念，不断前行，才能遇见更好的自己。
+---
+
